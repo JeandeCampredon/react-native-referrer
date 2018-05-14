c4ad18f7281d07efd304c7eaba778abf15258445
Christian Haug <csh@christianhaug.com>
2018-05-14 15:54:09 +0200

    Use SharedPreferences instead of private var to store referrerString

--- android/src/main/java/com/jdc/reactlibrary/ReferrerReceiver.java
+++ android/src/main/java/com/jdc/reactlibrary/ReferrerReceiver.java
@@ -4,21 +4,27 @@ package com.jdc.reactlibrary;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 public class ReferrerReceiver extends BroadcastReceiver {
-  private static String referrer = "";
+  public static final String REFERRER_PREFS_NAME = "referrerPreferences";
 
-  public static String getReferrer() {
-    return referrer;
+  public static String getReferrerFromSharedPreferences(Context context) {
+    SharedPreferences prefs = context.getSharedPreferences(REFERRER_PREFS_NAME, context.MODE_PRIVATE);
+    String storedReferrerString = prefs.getString("referrer", null);
+    return storedReferrerString;
   }
 
-  private static void setReferrer(String value) {
-    referrer = value;
+  private static void storeReferrerInSharedPreferences(String value, Context context) {
+    SharedPreferences.Editor editor = context.getSharedPreferences(REFERRER_PREFS_NAME, context.MODE_PRIVATE).edit();
+    editor.putString("referrer", value);
+    editor.apply();
   }
 
   @Override
   public void onReceive(Context context, Intent intent) {
     String referrerString = intent.getStringExtra("referrer");
-    setReferrer(referrerString);
+
+    storeReferrerInSharedPreferences(referrerString, context);
   }
 }
